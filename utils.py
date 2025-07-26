a2ff88ab30356c7bc00a35b4957cab006f380803
threatthreat <grezy8007@gmail.com>
2025-07-26 09:09:35 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, IS_VERIFY , START_IMG
+from info import AUTH_CHANNELS, LONG_IMDB_DESCRIPTION, IS_VERIFY, START_IMG
 from imdb import Cinemagoer
 import asyncio
 from pyrogram.types import Message
