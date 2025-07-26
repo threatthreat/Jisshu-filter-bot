e34942a81c2fa5801734ee7cc45e9637bc4187d2
threatthreat <grezy8007@gmail.com>
2025-07-26 09:45:19 -0700

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -1,12 +1,12 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest
 from database.users_chats_db import db
-from info import ADMINS, AUTH_CHANNEL
+from info import ADMINS, AUTH_CHANNELS
 
 # Accept join requests for multiple AUTH_CHANNELs
-@Client.on_chat_join_request()
+@Client.on_chat_join_request(filters.chat(AUTH_CHANNELS))
 async def join_reqs(client, message: ChatJoinRequest):
-    if message.chat.id in AUTH_CHANNEL:
+    if message.chat.id in AUTH_CHANNELS:  # ✅ fixed here
         if not await db.find_join_req(message.from_user.id):
             await db.add_join_req(message.from_user.id)
 
